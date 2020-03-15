10cc659e21d0766346418e1bcb5b8ad7f87a7234
Dakota States <dakota.states@gmail.com>
2020-03-15 01:08:50 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -8,3 +8,11 @@
 # In this change method, create columns with the correct names and 
 # value types according to the spec
 
+class CreateCostumes < ActiveRecord::Migration[5.2]
+  def change
+    create_table :costumes do |t|
+      t.string :name
+    end
+    add_column :costumes, :price, :integer
+  end
+end
