0c46637eccdf6f1cba2ef994839ff8a6fec9566b
Dakota States <dakota.states@gmail.com>
2020-03-15 01:54:05 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,1 +1,14 @@
 # Create your costume_stores migration here
+
+class CreateCostumes < ActiveRecord::Migration[5.2]
+  def change
+    create_table :costumes do |t|
+      t.string :name
+      t.float :price
+      t.string :size 
+      t.string :image_url
+      t.timestamp :created_at
+      t.timestamp :updated_at
+    end
+  end
+end
